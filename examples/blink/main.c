24b98ae236900715343b947f84e6a57c08add6bf
Barsy-Barsevich <victor.kinderknecht7@gmail.com>
2025-12-09 00:59:04 +0500

    try to create AH4 and PL1 compatible example, failed, I need to work aroung Makefile

--- examples/blink/main.c
+++ examples/blink/main.c
@@ -1,19 +1,36 @@
 #include "ah4-specific.h"
-#include "system_timer.h"
+#include "pl1-specific.h"
+#include "ah4-series.h"
 
+//#define AH4
+#define PL1
 
 int main()
 {
 	ah4_clock_config();
+
+#ifdef AH4
 	ah4_led_init();
-	system_timer_init();
+#elif defined( PL1 )
+	pl1_led_init();
+#endif
+
+	ah4_time_init();
 
 	while (1)
 	{
+#ifdef AH4
 		ah4_fsync_en_led_set(1);
-		system_timer_delay_ms(500);
+#elif defined( PL1 )
+		pl1_led_set(1);
+#endif
+		ah4_time_delay_ms(500);
+#ifdef AH4
 		ah4_fsync_en_led_set(0);
-		system_timer_delay_ms(500);
+#elif defined( PL1 )
+		pl1_led_set(0);
+#endif
+		ah4_time_delay_ms(500);
 	}
 
     return 0;
