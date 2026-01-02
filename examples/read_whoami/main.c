925bca65bdc470cb55337366aef82e10c92c4207
Barsy-Barsevich <victor.kinderknecht7@gmail.com>
2026-01-03 01:35:31 +0500

    examples update

--- examples/read_whoami/main.c
+++ examples/read_whoami/main.c
@@ -1,38 +1,22 @@
-#include <stdio.h>
-#include "ch32v30x_gpio.h"
+#include "ah4-specific.h"
+#include "ah4-series.h"
+#include "ah4-usbd.h"
+#include "icm45686_barsotion.h"
+
 
 int main()
 {
-	SystemCoreClockUpdate();
-    GPIO_InitTypeDef GPIO_InitStructure = { 0 };
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
-	GPIO_Init(GPIOC, &GPIO_InitStructure);
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
-	GPIO_Init(GPIOC, &GPIO_InitStructure);
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
-	GPIO_Init(GPIOC, &GPIO_InitStructure);
+	ah4_clock_config();
+	ah4_led_init();
+	ah4_time_init();
+	ah4_usb_init();
 
-    GPIO_WriteBit(GPIOC, GPIO_Pin_2, 1);
-
-    RCC->APB2PCENR |= RCC_APB2Periph_GPIOC;
-
-
-
-    while (1);
+	while (1)
+	{
+		ah4_fsync_en_led_set(1);
+		ah4_time_delay_ms(500);
+		ah4_fsync_en_led_set(0);
+		ah4_time_delay_ms(500);
+	}
     return 0;
 }
